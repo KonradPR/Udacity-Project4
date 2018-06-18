153ec6c91f3ca11734af79b34b6a37e9028e08ad
Konrad Przewłoka <konrad.przewloka@gmail.com>
2018-06-18 23:59:04 +0200

    feat:Added README

--- README.md
+++ README.md
@@ -1,6 +1,56 @@
-frontend-nanodegree-arcade-game
-===============================
+#Arcade Game Clone
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+##Table of contents
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+* [Introduction](#introduction)
+* [Enemy Object](#enemy-object)
+* [Player Obejct](#player-object)
+
+
+## Introduction
+
+This is a clone of a simple arcade game the goal of which is for player's
+character to reach the top end of the map while avoiding hitting Enemies
+
+## Enemy object
+Enemy object is a JS object used to represent on screen enemies. One instance
+of object represents one enemy. The constructor takes in two paramters, in order:
+xCord - sets x coordinate of the Enemy
+yCord - sets y coordinate of the Enemy
+Object has listed methods:
+.update(dt) - takes in dt which is delt of time between game ticks, this method
+updates the position of object
+.render() - draws the enemy on screen
+Each instance of Enemy object has it own version of two properties:
+.x - the x coordinate of the object
+.y - the y coordinate of the object
+There are also properties of the object's prototype that are shared by all
+instances of the Enemy object that are listed below:
+.sprite - image that is drawn on screen to represent object
+.speed - changes how fast the object moves
+.width - objects width
+.height - objects height
+
+## Player object
+Player object is a JS object used to represent player's character.
+The object's constructor takes in no paramters.
+Object has listed properties:
+.sprite - image that is drawn on screen to represent object
+.speed - changes how fast the object moves
+.width - objects width
+.height - objects height
+.x - the x coordinate of the object
+.y - the y coordinate of the object
+.direction - the direction in which the player is moving
+Object has listed methods:
+.update(dt) - takes in dt which is delt of time between game ticks, this method
+updates the position of object it also calls checkWin and checkCollision methods
+in order to monitor the state of the game
+.render() - draws the player on screen
+.reset() - sets plyer's position to the starting one
+.checkCollision() - checks if player has colided witch an enemy and if this
+ocours calls the reset method
+.checkWin() - checks if player has reached the top of tha map and if this ocours
+calls the reset method
+.handleInput(key) - sets the direction in which the player is moving based
+on direction that is given 
--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 // Enemies our player must avoid:
 //Paramters: xCord, setes x coordinate of the Enemy
 //yCord stes y coordinate of the Enemy
-var Enemy = function(xCord,yCord) {
-    this.sprite = 'images/enemy-bug.png';
-    this.x = xCord;
-    this.y = yCord;
-    this.speed = 80;
+var Enemy = function(xCord, yCord) {
+  this.x = xCord;
+  this.y = yCord;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    this.x += this.speed*dt;
-    if (this.x>=505) {
-      this.x = -this.width;
-    }
+  this.x += this.speed * dt;
+  if (this.x >= 505) {
+    this.x = -this.width;
+  }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-//Those variables are used for colision detection
+//Those variables are used for colision detection and enemy movement
 Enemy.prototype.width = 80;
 Enemy.prototype.height = 64;
+Enemy.prototype.speed = 80;
+Enemy.prototype.sprite = 'images/enemy-bug.png';
 
 //Player object is used to represent player on screen
 var Player = function() {
@@ -39,7 +39,7 @@ var Player = function() {
 
 //Draw the player on the screen, required method for game
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 //Resets player position to the starting one
@@ -52,12 +52,12 @@ Player.prototype.reset = function() {
 //and if it ocours resets player's position
 Player.prototype.checkCollision = function() {
   allEnemies.forEach(item => {
-    if(((this.x>=item.x&&this.x<=item.x+item.width)||
-    (this.x+this.width>=item.x&&this.x+this.width<=item.x+item.width))&&
-    ((this.y<=item.y&&this.y>=item.y-item.height)||
-    (this.y-this.height<=item.y&&this.y-this.height>=item.y-item.height)||
-    (this.y-(this.height/2)<=item.y&&this.y-(this.height/2)>=item.y-item.height))) {
-        this.reset();
+    if (((this.x >= item.x && this.x <= item.x + item.width) ||
+        (this.x + this.width >= item.x && this.x + this.width <= item.x + item.width)) &&
+      ((this.y <= item.y && this.y >= item.y - item.height) ||
+        (this.y - this.height <= item.y && this.y - this.height >= item.y - item.height) ||
+        (this.y - (this.height / 2) <= item.y && this.y - (this.height / 2) >= item.y - item.height))) {
+      this.reset();
     }
   });
 };
@@ -65,7 +65,7 @@ Player.prototype.checkCollision = function() {
 //Checks if player has reached the top end of the map
 //and if it ocours resets player position
 Player.prototype.checkWin = function() {
-  if (this.y<=0) {
+  if (this.y <= 0) {
     this.reset();
   };
 };
@@ -75,45 +75,45 @@ Player.prototype.checkWin = function() {
 //this method also calls checkCollision
 //and checkWin methods
 Player.prototype.update = function(dt) {
-  if (this.direction==='left'&&this.x>=-15) {
-      this.x-=this.speed*dt;
-  } else if (this.direction==='right'&&this.x<=505-88) {
-      this.x+=this.speed*dt;
-  } else if (this.direction==='up'&&this.y>=0) {
-      this.y-=this.speed*dt;
-  } else if (this.direction==='down'&&this.y<=606-201) {
-      this.y+=this.speed*dt;
+  if (this.direction === 'left' && this.x >= -15) {
+    this.x -= this.speed * dt;
+  } else if (this.direction === 'right' && this.x <= 505 - 88) {
+    this.x += this.speed * dt;
+  } else if (this.direction === 'up' && this.y >= 0) {
+    this.y -= this.speed * dt;
+  } else if (this.direction === 'down' && this.y <= 606 - 201) {
+    this.y += this.speed * dt;
   }
 
-this.checkCollision();
+  this.checkCollision();
 
-this.checkWin();
+  this.checkWin();
 };
 
 //This method sets the direction in which player is moving
 //based on key input
 Player.prototype.handleInput = function(key) {
 
-  if (key==='left') {
-      this.direction = 'left'
-  } else if (key==='right') {
-      this.direction = 'right'
-  } else if (key==='up') {
-      this.direction = 'up'
-  } else if (key==='down') {
-      this.direction = 'down'
+  if (key === 'left') {
+    this.direction = 'left'
+  } else if (key === 'right') {
+    this.direction = 'right'
+  } else if (key === 'up') {
+    this.direction = 'up'
+  } else if (key === 'down') {
+    this.direction = 'down'
   }
 };
 
 //Creation of game objects Enemies are stored in allEnemies Array
 //contents of the array are created using an IIFE
-const allEnemies = (function(){
+const allEnemies = (function() {
   const arr = [];
   let x, y;
-  for(let i = 0; i < 6; i++) {
-      x = 100*i;
-      y = 50*i;
-      arr[i] = new Enemy(x,y);
+  for (let i = 0; i < 6; i++) {
+    x = 100 * i;
+    y = 50 * i;
+    arr[i] = new Enemy(x, y);
   }
   return arr;
 }());
@@ -123,12 +123,12 @@ const player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
